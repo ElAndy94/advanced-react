b4af39ddbe2773c28a9c77c0199c8fb144dfa31d
Andrew <andrewpeliza@hotmail.com>
2019-06-07 18:47:35 +0100

    further tests using Enzyme and changing routes to absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
-import App from '../App';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-
-  ReactDOM.render(<App />, div);
-
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  //   console.log(div.innerHTML);
-  expect(div.innerHTML).toContain('Comment Box');
-  expect(div.innerHTML).toContain('Comment List');
-
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
+
+// it('shows a comment box', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   // Looks inside the div
+//   // and checks to see if the CommentBox is in there
+//   // console.log(div.innerHTML);
+//   expect(div.innerHTML).toContain('Comment Box');
+//   expect(div.innerHTML).toContain('Comment List');
+//   ReactDOM.unmountComponentAtNode(div);
+//   });
