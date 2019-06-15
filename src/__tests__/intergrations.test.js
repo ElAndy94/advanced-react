ff5aa2df209df87c70cb872d46f27f85a4898e08
Andrew <andrewpeliza@hotmail.com>
2019-06-15 15:39:11 +0100

    Mocking an axios call with moxios - Integration Tests done.

--- src/__tests__/intergrations.test.js
+++ src/__tests__/intergrations.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', done => {
   // Attempt to render the entire app
   const wrapped = mount(
     <Root>
@@ -15,5 +28,13 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to find a list of comments
-  expect(wrapped.find('li').length).toEqual(500);
+  moxios.wait(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+
+    wrapped.unmount();
+  });
 });
